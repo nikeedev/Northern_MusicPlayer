9ab4e44c8a32fb84b9780e0f29c3e84b18c75c92
nikeedev <myftr12394@gmail.com>
2022-02-17 11:14:03 +0100

    updates

--- script.js
+++ script.js
@@ -8,15 +8,23 @@ var CustomFile = document.getElementById("CustomMusicFile");
 var AcceptCustomMusic = document.getElementById("AcceptCustomMusic");
 var ChangeCustomMusic = document.getElementById("ChangeCustomMusic");
 CustomFile.style.display = "none";
+
+
 var CurrentNumberOfMusic = 0;
+
 var PressedPlay = false;
+
 var ListOfMusics = [
     "test.mp3",
     "https://samplelib.com/lib/preview/mp3/sample-15s.mp3",
-];
+    
+]
+
 audio.controls = false;
 audio.src = ListOfMusics[CurrentNumberOfMusic];
-function play() {
+
+function play()
+{
     if (!PressedPlay) {
         PlayButton.innerHTML = "I I";
         PressedPlay = true;
@@ -24,11 +32,12 @@ function play() {
     }
     else if (PressedPlay) {
         PlayButton.innerHTML = "&#9654;";
-        PressedPlay = true;
+        PressedPlay = false;
         audio.pause();
     }
-    info.innerHTML = "Current song/music playing: " + (CurrentNumberOfMusic + 1 + "." + audio.src);
+    info.innerHTML = "Current song/music playing: " + (CurrentNumberOfMusic + 1 + "." + audio.src)
 }
+
 function previousFunc() {
     CurrentNumberOfMusic -= 1;
     audio.src = ListOfMusics[CurrentNumberOfMusic];
@@ -37,8 +46,9 @@ function previousFunc() {
     PressedPlay = true;
     PlayButton.innerHTML = "I I";
     audio.play();
-    info.innerHTML = "Current song/music playing: " + (CurrentNumberOfMusic + 1 + ". " + audio.src);
+    info.innerHTML = "Current song/music playing: " + (CurrentNumberOfMusic + 1 + ". " + audio.src)
 }
+
 function nextFunc() {
     CurrentNumberOfMusic += 1;
     audio.currentTime = 0;
@@ -47,30 +57,27 @@ function nextFunc() {
     PressedPlay = true;
     PlayButton.innerHTML = "I I";
     audio.play();
-    info.innerHTML = "Current song/music playing: " + (CurrentNumberOfMusic + 1 + ". " + audio.src);
+    info.innerHTML = "Current song/music playing: " + (CurrentNumberOfMusic + 1 + ". " + audio.src)
 }
+
+
 function UseCustomMusic() {
-    CurrentNumberOfMusic = ListOfMusics.length + 1;
-    ListOfMusics.push(String(CustomUrl.innerHTML));
-    audio.src = ListOfMusics[CurrentNumberOfMusic];
+    ListOfMusics.push(String(CustomUrl.value));
     audio.load();
-    audio.currentTime = 0;
-    PressedPlay = true;
-    PlayButton.innerHTML = "I I";
-    audio.play();
-    info.innerHTML = "Current song/music playing: " + (CurrentNumberOfMusic + 1 + ". " + audio.src);
 }
+
+
 function ChangeCustomMusicType() {
     if (CustomUrl.style.display != "none") {
         CustomUrl.style.display = "none";
         CustomUrl.innerHTML = null;
         CustomFile.style.display = "block";
-        ChangeCustomMusic.innerHTML = "Use from URL/path instead?";
+        ChangeCustomMusic.innerHTML = "Use from URL/path instead?"
     }
     else if (CustomUrl.style.display == "none") {
         CustomUrl.style.display = "block";
         CustomFile.innerHTML = null;
         CustomFile.style.display = "None";
-        ChangeCustomMusic.innerHTML = "Open from file instead?";
+        ChangeCustomMusic.innerHTML = "Open from file instead?"
     }
 }
